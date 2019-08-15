2b5dfc940c9f5c58faa87321045d3d4a160211c9
mihir <phatak.mihir@gmail.com>
2019-08-14 21:55:27 -0400

    ../../

---
+++ arrays-and-strings/9/string-rotation.cpp
@@ -0,0 +1,28 @@
+#include<hash>
+#include<string>
+#include<vector>
+	
+	/* Concatenate two strings to check with original string */
+
+	int main()
+
+
+	{
+	std::string s1 = "waterbottle";
+	std::string s2 = "erbottlewat"; 
+
+	bool isSubstring()
+	{
+		
+
+	}
+
+	std::string s3 = s2 + s2;
+	if(isSubstring(s1,s3))
+	{
+		return true;
+	}
+
+
+	return 0;
+	}
