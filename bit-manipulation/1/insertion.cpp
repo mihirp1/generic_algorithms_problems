bd4911f923108f5a2141179a99c8bc617e5cb937
mihir <phatak.mihir@gmail.com>
2019-08-24 16:45:49 -0400

    Insert one integer inside another

---
+++ bit-manipulation/1/insertion.cpp
@@ -0,0 +1,51 @@
+//CTCI ED 6 : Chp 5, Problem #1
+#include<iostream>
+#include<bitset>
+
+	int main(int argc, char* argv[])
+
+	{
+	
+	int M = 2048;
+	int N = 190;
+
+	int start = 0;
+	int end = 9;
+	int main_mask = 0;	
+
+	//std::cout<<std::bitset<32>(0xFFFFFFFF)<<std::endl;
+
+	//Create A Mask
+	int mask = 0xFFFFFFFF;
+	//std::cout << mask<<std::endl;	
+	
+
+
+	for(int i = end; i > start-1 ; --i)
+	{
+		main_mask += 1 << i;
+
+	}
+
+	std::cout<<std::bitset<32>(main_mask)<<std::endl;
+
+	main_mask = ~main_mask;
+	std::cout<<std::bitset<32>(main_mask)<<std::endl;	
+
+	int changed_number;
+
+	changed_number = M & main_mask;
+	std::cout<<std::bitset<32>(changed_number)<<std::endl;
+
+
+	auto ans = changed_number | N;
+
+	std::cout<<"M = "<<std::bitset<32>(M)<<std::endl;
+	std::cout<<"N = "<<std::bitset<32>(N)<<std::endl;
+
+	std::cout<<"A = "<<std::bitset<32>(ans)<<std::endl;
+
+	return 0;
+	}
+
+
