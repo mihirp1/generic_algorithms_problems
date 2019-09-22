d4022431c3df49f4a14ac3f84cf42f3604816bd8
mihir <phatak.mihir@gmail.com>
2019-09-22 17:55:10 -0400

    n queens resources

---
+++ recursion-and-dynamic-programming/12/solution.cpp
@@ -0,0 +1,5 @@
+//https://www.youtube.com/watch?v=jJPtLzq1E-Y
+//https://www.geeksforgeeks.org/n-queen-problem-backtracking-3/
+//https://leetcode.com/problems/n-queens/discuss/19808/Accepted-4ms-c%2B%2B-solution-use-backtracking-and-bitmask-easy-understand.
+//https://www.youtube.com/watch?v=yQ84lk-EXTQ
+
